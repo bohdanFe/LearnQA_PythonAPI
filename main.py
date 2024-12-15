b619c9f9d24cef55da399d41a9a042ea7c835fd3
Bohdan Fedorenko <bfedorenko@luxoft.com>
2024-12-15 17:15:51 +0200

    Add try exept test for api get_text

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from json.decoder import JSONDecodeError
 import requests
 
-payload = {"name":"User"}
-response = requests.get("https://playground.learnqa.ru/api/hello", verify=False, params=payload)
-
+response = requests.get("https://playground.learnqa.ru/api/get_text", verify=False)
 print(response.text)
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text)
+
+except JSONDecodeError:
+    print("Response is not a JSOn format")
