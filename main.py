7d1a7e9ab642370fd43d5f7540092aa275be3189
Bohdan Fedorenko <bfedorenko@luxoft.com>
2024-12-15 20:50:39 +0200

    Add exemple test 301 status code

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import requests
 
-response = requests.post("https://playground.learnqa.ru/api/check_type", verify=False, data={"params1":"value1"})
-print(response.text)
+response = requests.post("https://playground.learnqa.ru/api/get_301", verify=False, allow_redirects=True)
+first_response = response.history[0]
+second_request = response
+
+print(first_response.url)
+print(second_request.url)
