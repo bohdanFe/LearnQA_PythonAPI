ac2f415b7a4a22da1cc794689566dd4bcee2305d
Bohdan Fedorenko <bfedorenko@luxoft.com>
2024-12-15 17:40:20 +0200

    Testing data post method

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from json.decoder import JSONDecodeError
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text", verify=False)
+response = requests.post("https://playground.learnqa.ru/api/check_type", verify=False, data={"params1":"value1"})
 print(response.text)
-try:
-    parsed_response_text = response.json()
-    print(parsed_response_text)
-
-except JSONDecodeError:
-    print("Response is not a JSOn format")
