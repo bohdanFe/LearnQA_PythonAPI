bce3d71a28882ca7642b1b2105f59849bab609ae
Bohdan Fedorenko <bfedorenko@luxoft.com>
2024-12-18 16:07:14 +0200

    Add req file, add test_examples file, work with pytest

---
+++ test_examples.py
@@ -0,0 +1,11 @@
+class TestExaple:
+    def test(self):
+        a = 5
+        b = 9
+        expected_sum = 14
+        assert a+b == expected_sum, f"Sum of variables a and b is not equal to {expected_sum}"    
+    def test2(self):
+        a = 5
+        b = 8
+        expected_sum = 14
+        assert a+b == expected_sum, f"Sum of variables a and b is not equal to {expected_sum}"  
